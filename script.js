243af41c1c260b5be3713393feaf218eabf28029
MLPBK07 <javilanderos881@gmail.com>
2025-02-12 17:46:04 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesBtn").addEventListener("click", function() {
+    document.getElementById("response").innerText = "¡Sabía que dirías que sí! ❤️";
+});
+
+document.getElementById("noBtn").addEventListener("click", function() {
+    alert("¡Oh no! 😢 Inténtalo de nuevo...");
+});
